eb0a8202db8ad9f1182579fe96d5b5078746b314
Dainius <100964434+DainiusMI@users.noreply.github.com>
2022-03-30 21:15:50 +0300

    broken JS

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-let score;
+
+function voting() {
+let score = document.activeElement.textContent;
+document.getElementById("score").innerText = score;
+//document.getElementsByClassName("voting-score").innerText = score;
+}
+
+const buttonSubmit = document.getElementsByClassName("submit");
+const votingForm = document.getElementsByClassName("rating-form");
+const  thankingForm = document.getElementsByClassName("thanking-form");
+
+
+buttonSubmit.addEventListener("click", switchForms());
+
+function switchForms() {
+
+    votingForm.classList.add('hide-form');
+
+    thankingForm.classList.add('display-form');
+}
