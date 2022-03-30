1860aa8d921fda31b9db96d38c413218b0ce8d7f
Dainius <100964434+DainiusMI@users.noreply.github.com>
2022-03-30 22:32:52 +0300

    JS complete I think

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 
+
 function voting() {
-let score = document.activeElement.textContent;
+const score = document.activeElement.textContent;
 document.getElementById("score").innerText = score;
-//document.getElementsByClassName("voting-score").innerText = score;
 }
 
-const buttonSubmit = document.getElementsByClassName("submit");
-const votingForm = document.getElementsByClassName("rating-form");
-const  thankingForm = document.getElementsByClassName("thanking-form");
 
-
-buttonSubmit.addEventListener("click", switchForms());
-
-function switchForms() {
-
-    votingForm.classList.add('hide-form');
-
-    thankingForm.classList.add('display-form');
-}
+function submit() {
+    var score = document.getElementById("score").innerText;
+    if (score > 0) {
+        var ratingForm = document.getElementById("rating-form");
+        ratingForm.classList.add("hide-form");
+        var tankingForm = document.getElementById("thanking-form");
+        tankingForm.classList.add("display-form");
+    }
+    else alert('Please vote before submiting');
+ }
